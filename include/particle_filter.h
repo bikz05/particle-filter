99c7d11062fae8fc270202fc651aa8401d643d6f
bikz05 <bikz.05@gmail.com>
2016-10-24 01:10:42 -0400

    Variance Sampling added,

--- include/particle_filter.h
+++ include/particle_filter.h
@@ -10,6 +10,7 @@
 #include "map.h"
 #include <random>
 #include <utility>
+#include <ctime>
 
 namespace str{
 
@@ -20,9 +21,12 @@ class ParticleFilter{
 		MeasurementModel measurementModel_;
 		int no_samples_;
 		std::vector<str::Pose<T>> samples_;
-		std::vector<str::Pose<T>> samples_temp_;
+		std::vector<str::Pose<T>> samplesTemp_;
 		std::vector<T> weights_;
 		std::random_device rd_;
+
+		void importanceSampling();
+		void lowVarianceSampling();
 	public:
 		ParticleFilter(int no_samples);
 		std::vector<str::Pose<T>>& mcl(const std::vector<str::Pose<T>>& x_tm1,
@@ -30,8 +34,7 @@ class ParticleFilter{
 			const str::LaserReading<T>& z_t);
 		std::vector<str::Pose<T>>& predict(const std::vector<str::Pose<T>>& x_tm1,
 			const std::pair<str::OdometryReading<T>, str::OdometryReading<T>>& odoReadingPair);
-		std::vector<str::Pose<T>>& update(const str::LaserReading<T>& z_t);
-
+		std::vector<str::Pose<T>>& update(const str::LaserReading<T>& z_t, int sampling_type=0);
 };
 
 }
--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -1,8 +1,8 @@
 #include "../include/particle_filter.h"
 
 template <typename T>
-str::ParticleFilter<T>::ParticleFilter(int no_samples):no_samples_(no_samples_), samples_(no_samples, str::Pose<double>()),
-	samples_temp_(no_samples, str::Pose<double>()), weights_(std::vector<T>(no_samples, 1)){
+str::ParticleFilter<T>::ParticleFilter(int no_samples):no_samples_(no_samples), samples_(no_samples, str::Pose<double>()),
+	samplesTemp_(no_samples, str::Pose<double>()), weights_(std::vector<T>(no_samples, 1)){
 }
 
 template <typename T>
@@ -13,34 +13,68 @@ std::vector<str::Pose<T>>& str::ParticleFilter<T>::mcl(const std::vector<str::Po
 	// This is needed in case we use adaptive sampling.
 	/**
 	this->no_samples_ = x_tm1.size();
-	this->samples_temp_ = std::vector<T>(this->no_samples_);
+	this->samplesTemp_ = std::vector<T>(this->no_samples_);
 	this->samples_ = std::vector<T>(this->no_samples_);
 	**/
 
 	this->predict(x_tm1, odoReadingPair);
-	this->update(z_t);
+	this->update(z_t, 1);
 	return this->samples_;
 }
 
 template <typename T>
 std::vector<str::Pose<T>>&  str::ParticleFilter<T>::predict(const std::vector<str::Pose<T>>& x_tm1,
 		const std::pair<str::OdometryReading<T>, str::OdometryReading<T>>& odoReadingPair){
-	for(int m = 0; m < this->samples_temp_.size(); m++){
+	for(int m = 0; m < this->samplesTemp_.size(); m++){
 		// Sample from the motion model
-		this->samples_temp_[m] = motionModel_.Sample(odoReadingPair, x_tm1[m]);
+		this->samplesTemp_[m] = motionModel_.Sample(odoReadingPair, x_tm1[m]);
 		// TODO: Sample from the measurement model
-		// this->weights_[m] = measurementModel_.getProbFromBeamModel(this->samples_temp_[m]);
+		// this->weights_[m] = measurementModel_.getProbFromBeamModel(this->samplesTemp_[m]);
 	}
-	return this->samples_temp_;
+	return this->samplesTemp_;
 }
 
 template <typename T>
-std::vector<str::Pose<T>>&  str::ParticleFilter<T>::update(const str::LaserReading<T>& z_t){
-	std::mt19937 gen(rd_());
-	std::discrete_distribution<> d(this->weights_.begin(), this->weights_.end());
-	for(int m = 0; m < this->samples_temp_.size(); m++){
-		int sample_id = d(gen);
-		this->samples_[m] = this->samples_temp_[sample_id];
+std::vector<str::Pose<T>>&  str::ParticleFilter<T>::update(const str::LaserReading<T>& z_t, int samplingType){
+
+	// Perform the samlping
+	if(samplingType == 0){
+		importanceSampling();
+	}
+	else if(samplingType == 1){
+		lowVarianceSampling();
 	}
 	return this->samples_;
+}
+
+template <typename T>
+void str::ParticleFilter<T>::importanceSampling(){
+	std::mt19937 gen(rd_());
+	std::discrete_distribution<> d(this->weights_.begin(), this->weights_.end());
+	for(int m = 0; m < this->samplesTemp_.size(); m++){
+		int sample_id = d(gen);
+		this->samples_[m] = this->samplesTemp_[sample_id];
+	}
+}
+
+template <typename T>
+void str::ParticleFilter<T>::lowVarianceSampling(){
+	// Normalize the weights
+	double n = std::accumulate(this->weights_.begin(), this->weights_.end(), 0);
+
+	std::srand(std::time(0));
+	std::cout << no_samples_;
+
+	double r = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX) / this->no_samples_;
+	double c = this->weights_[0] / n;
+	int i = 0;
+
+	for(int m = 0; m < this->samplesTemp_.size(); m++){
+		double U = r + 1.0 * m / this->no_samples_;
+		while(U > c){
+			i++;
+			c += this->weights_[i] / n;
+		}
+		this->samples_[m] = this->samplesTemp_[i];
+	}
 }
