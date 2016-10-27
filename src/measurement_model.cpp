ccd694949862bd3322085b75709abcac0bbc2153
shukailin <shu.kaikai@gmail.com>
2016-10-27 19:57:36 -0400

    fix segmentation fault because of accessing boundary coord

--- src/measurement_model.cpp
+++ src/measurement_model.cpp
@@ -27,22 +27,40 @@ double str::MeasurementModel::getProbability(const str::LaserReading<double>& la
 	//Step1. get pre-cashing table using pose, should return 180 values
 	//Step2. using table value and laser reading to compute prob using this->getProbFromBeamModel
 	//Step3. calculate overall probability, product of all 180 measurements
-
-	double overall_prob = 1;
-	std::vector<double> predict_distance;
-	this->getDistTableByPose(pose, predict_distance);
-	std::vector<double> laser_data = laser_reading.getRanges();
-	
-	for (int i = 0; i < 180; ++i)
+	//std::cout<<"pose = "<<pose;
+	//std::cout<<pose.getX()<<" "<<pose.getY()<<" "<<pose.getTheta()<<std::endl;
+	str::Pose<double> laser_pose = this->getSensorPose(pose);
+	if(laser_pose.getX() < 0 || laser_pose.getX() > MAP_X_SIZE_IN_CM || laser_pose.getY() < 0 || laser_pose.getY() > MAP_Y_SIZE_IN_CM)
 	{
-		// get z_t_k* using pose and measurement idx
-		double z_t_k_star = predict_distance.at(i);
-
-		double prob = this->getProbFromBeamModel( laser_data[i] ,z_t_k_star);
-		overall_prob *= prob;
+		std::cout<<"pose out of boundary, return 0 probability"<<std::endl;
+		return 0;
 	}
+	//std::cout<<laser_pose.getX()<<" "<<laser_pose.getY()<<" "<<laser_pose.getTheta()<<std::endl;
 
-	return overall_prob;
+	//double overall_prob = 1;
+	double overall_prob = 0;
+	std::vector<double> predict_distance;
+	this->getDistTableByPose(laser_pose, predict_distance);
+	
+	std::vector<double> laser_data = laser_reading.getRanges();
+	for (int i = 0; i < 180; i+=2)
+	{
+		
+		
+		// get z_t_k* using pose and measurement idx
+		if((i > 0 && i <= 60) || (i > 120 && i < 180))
+		{
+			// get z_t_k* using pose and measurement idx
+			//std::cout<<i<<std::endl;
+			double z_t_k_star = predict_distance.at(i);
+
+			double prob = this->getProbFromBeamModel( laser_data[i] ,z_t_k_star);
+			//overall_prob *= prob;
+			overall_prob += std::log(prob);
+		}
+	}
+	//return overall_prob;
+	return exp(overall_prob);
 }
 
 /**
@@ -50,14 +68,12 @@ double str::MeasurementModel::getProbability(const str::LaserReading<double>& la
 **/
 void str::MeasurementModel::getDistTableByPose(const str::Pose<double>& pose, std::vector<double>& predict_distance)
 {	
-	//std::cout<<pose.getX()<<" "<<pose.getY()<<" "<<pose.getTheta()<<std::endl;
-	str::Pose<double> laser_pose = this->getSensorPose(pose);
-	//std::cout<<laser_pose.getX()<<" "<<laser_pose.getY()<<" "<<laser_pose.getTheta()<<std::endl;
+	str::Pose<double> laser_pose = pose;
 
 	str::Pose<unsigned int> laser_pose_grid;
 	this->poseCoordToGrid(laser_pose, laser_pose_grid);//this function transfer theta to degree
 
-
+	//std::cout<<"laser grid ="<<laser_pose_grid<<std::endl;
 	unsigned int x = laser_pose_grid.getX(); 
 	unsigned int y = laser_pose_grid.getY();
 	unsigned int theta = laser_pose_grid.getTheta();
@@ -102,7 +118,7 @@ void str::MeasurementModel::selectDistTableByTheta(const unsigned int& theta_in_
 		//std::cout<<"case 1"<<std::endl;
 		unsigned int start_idx = theta_start;
 		unsigned int end_idx = theta_end;
-		std::cout<<start_idx<<" "<<end_idx<<std::endl;
+		//std::cout<<start_idx<<" "<<end_idx<<std::endl;
 		if( (end_idx - start_idx + 1) != 180)
 			std::cout<<"wrong index in section 1"<<std::endl;
 		std::vector<double> selected_dist_table(dist_table_per_grid.begin()+start_idx, dist_table_per_grid.begin()+end_idx+1);
@@ -157,13 +173,19 @@ void str::MeasurementModel::selectDistTableByTheta(const unsigned int& theta_in_
 **/
 void str::MeasurementModel::poseCoordToGrid(const str::Pose<double>& pose_coord, str::Pose<unsigned int>& pose_grid)
 {
-	if(pose_coord.getX()<0 || pose_coord.getY() <0)
+	if(pose_coord.getX()<0 || pose_coord.getX() >= MAP_X_SIZE_IN_CM || pose_coord.getY() <0 || pose_coord.getY() >= MAP_Y_SIZE_IN_CM)
 		std::cout<<"Error: coord out of range coord: "<<pose_coord.getX()<<" "<<pose_coord.getY()<<std::endl;
 
 
 	str::Pose<double> copy_pose = pose_coord;
-	pose_grid.setX(static_cast<unsigned int>(round(copy_pose.getX()/MAP_RESOLUTION)));
-	pose_grid.setY(static_cast<unsigned int>(round(copy_pose.getY()/MAP_RESOLUTION)));
+	unsigned int x = static_cast<unsigned int>(round(copy_pose.getX()/MAP_RESOLUTION));
+	if(x >= MAP_X_SIZE_IN_GRID) x = MAP_X_SIZE_IN_GRID - 1;
+	pose_grid.setX(x);
+	
+	unsigned int y = static_cast<unsigned int>(round(copy_pose.getY()/MAP_RESOLUTION));
+	if(y >= MAP_Y_SIZE_IN_GRID) y = MAP_Y_SIZE_IN_GRID - 1;
+	pose_grid.setY(y);
+
 	//std::cout<<"theta = "<<copy_pose.getTheta()<<std::endl;
 	// if( copy_pose.getTheta() < 0)
 	// 	std::cout<<"currently it did not support negtive theta: "<<copy_pose.getTheta()<<std::endl;
@@ -188,9 +210,10 @@ double str::MeasurementModel::getProbFromBeamModel(const double& measurement, co
 	double w_p_max = params_.w_max * this->prob_max(measurement);
 	double w_p_rand = params_.w_rand * this->prob_rand(measurement);
 
-	//std::cout<<"prob: "<<w_p_hit + w_p_short + w_p_max + w_p_rand<<" measure: "<<measurement<<" predict: "<<predict_measurement<<" w_p_hit: "<<w_p_hit<<" w_p_short: "<<w_p_short<<" w_p_max: "<<w_p_max<<" w_p_rand: "<<w_p_rand<<std::endl;
+	//std::cout<<"prob: "<<std::log(w_p_hit + w_p_short + w_p_max + w_p_rand)<<" measure: "<<measurement<<" predict: "<<predict_measurement<<" w_p_hit: "<<w_p_hit<<" w_p_short: "<<w_p_short<<" w_p_max: "<<w_p_max<<" w_p_rand: "<<w_p_rand<<std::endl;
 
 	return w_p_hit + w_p_short + w_p_max + w_p_rand;
+	//return w_p_hit;
 }
 
 /**
@@ -208,6 +231,7 @@ double str::MeasurementModel::prob_hit(const double& measurement, const double& 
 {
 	double variance = params_.sigma_hit * params_.sigma_hit;
 	double eta = 1/sqrt(2 * M_PI * variance);
+	//double eta = 1;
 	double diff = measurement - predict_measurement;
 	double p_hit = eta*exp(-0.5 * diff * diff / variance);
 	return (measurement >= 0 && measurement <= z_max_) ? p_hit : 0.0;
@@ -228,7 +252,7 @@ double str::MeasurementModel::prob_short(const double& measurement, const double
 **/
 double str::MeasurementModel::prob_max(const double& measurement)
 {
-	return (measurement == z_max_) ? 1.0 : 0.0;
+	return (measurement >= z_max_) ? 1.0 : 0.0;
 }
 
 /**
@@ -253,13 +277,13 @@ void str::MeasurementModel::UnitTest()
 
 	unsigned int theta_in_grid = 90;
 	this->selectDistTableByTheta( theta_in_grid, dist_table_per_grid, coor_per_grid);
-	for(auto ele:dist_table_per_grid)
-	{
-		std::cout<<ele<<" ";
-	}
-	std::cout<<std::endl;
+	// for(auto ele:dist_table_per_grid)
+	// {
+	// 	std::cout<<ele<<" ";
+	// }
+	// std::cout<<std::endl;
 
-	std::cout<<"size = "<<dist_table_per_grid.size()<<std::endl;
+	//std::cout<<"size = "<<dist_table_per_grid.size()<<std::endl;
 
 
 
